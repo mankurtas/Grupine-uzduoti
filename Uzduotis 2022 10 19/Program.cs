9bfc5380148bf1e8e58c3dea20ba55ceca176f6c
mankurtas <povis.vasia@gmail.com>
2022-10-19 19:15:14 +0300

    Earth Volume Added

--- Uzduotis 2022 10 19/Program.cs
+++ Uzduotis 2022 10 19/Program.cs
@@ -1,8 +1,28 @@
-﻿// triangle C length
+﻿
+//Earth Volume
+
+
+int volume = 6371;
 
 
 
-double triangleC = (double) Math.Sqrt(15 * 15 + 10*10 );
+double earthVolume = (double)(4.0 / 3 * Math.PI * volume * volume * volume);
+
+
+
+Console.Write(earthVolume);
+
+// triangle C length
+double triangleC = (double) Math.Round(Math.Sqrt(15 * 15 + 10 * 10),2);
 
 Console.WriteLine(triangleC);
 
+
+
+//long sum = area + earthVolume + triangleC;
+//long average = sum /3;
+
+
+//Console.WriteLine(sum);
+
+//Console.WriteLine(average);

07252bfdb6d41e1bccc399ae8b1764ba25c9f9d6
Razeling <n.apulskis@gmail.com>
2022-10-19 19:17:34 +0300

    1 uzduotis

--- Uzduotis 2022 10 19/Program.cs
+++ Uzduotis 2022 10 19/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int area;
+double radius = 6371;
+
+area = (int)(Math.PI * Math.Pow(radius, 2));
+Console.WriteLine($"Earth's area is: {area} km");
